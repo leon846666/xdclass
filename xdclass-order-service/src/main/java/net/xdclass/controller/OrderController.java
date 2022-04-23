4ab71180c057fa1fe441dffe48e40be1f1d3a6c7
Yang <leon846666@gmail.com>
2022-04-23 17:30:31 +0800

    add alibaba nacos as register center

--- xdclass-order-service/src/main/java/net/xdclass/controller/OrderController.java
+++ xdclass-order-service/src/main/java/net/xdclass/controller/OrderController.java
@@ -3,11 +3,14 @@ package net.xdclass.controller;
 import net.xdclass.domain.Video;
 import net.xdclass.domain.VideoOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.List;
 
 @RequestMapping("api/v1/videoOrder")
 @RestController
@@ -15,9 +18,17 @@ public class OrderController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping("/save")
     public Object saveOrder(int videoId){
-        Video forObject = restTemplate.getForObject("http://localhost:9000/api/v1/video/find?id=" + videoId, Video.class);
+       // Video forObject = restTemplate.getForObject("http://localhost:9000/api/v1/video/find?id=" + videoId, Video.class);
+        List<ServiceInstance> list = discoveryClient.getInstances("xdclass-video-service");
+        ServiceInstance serviceInstance = list.get(0);
+        Video forObject = restTemplate.getForObject("http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/api/v1/video/find?id=" + videoId, Video.class);
+
 
         VideoOrder vo  = new VideoOrder();
         vo.setCreateTime(new Date());
--- xdclass-video-service/src/main/java/net/xdclass/VideoApplication.java
+++ xdclass-video-service/src/main/java/net/xdclass/VideoApplication.java
@@ -3,9 +3,11 @@ package net.xdclass;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("net.xdclass.dao")
+@EnableDiscoveryClient
 public class VideoApplication {
 
     public static void main(String[] args) {
